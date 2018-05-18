bed21bdc063d85fc661aaa2355be7434a36bfded
GigsFBB <39396049+GigsFBB@users.noreply.github.com>
2018-05-18 14:17:48 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xP4NQWEQCzw+/+93Fq2Ct3WbnLOhTj/IQLnqiNku4inVa45UnAfS61glvFdDIUriM6WCeAeuB7FfbYPO72WY5NL6lq1MhePgxFcD2TLYpHbxGaCiXAJvs1N1q4n2RmVgT1H8MfuGu80FzOh82+d5agdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
