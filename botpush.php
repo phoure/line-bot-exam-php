917ed460392baedc4357762519a3f8724b0af605
GigsFBB <39396049+GigsFBB@users.noreply.github.com>
2018-05-18 14:35:42 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xP4NQWEQCzw+/+93Fq2Ct3WbnLOhTj/IQLnqiNku4inVa45UnAfS61glvFdDIUriM6WCeAeuB7FfbYPO72WY5NL6lq1MhePgxFcD2TLYpHbxGaCiXAJvs1N1q4n2RmVgT1H8MfuGu80FzOh82+d5agdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '19e008e1a8e389b1d2e518e159026488';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U2f115cec459c73d35957e9ed1e4b3110';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
