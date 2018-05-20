d61355308c6ad09a33ba33a9f9580ec92507f864
phoure <phoure.capuchino@msn.com>
2018-05-20 20:54:21 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -16,6 +16,15 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->getMessageContent('7989246786688');
+
+
+
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
