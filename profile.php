34d3464c21b479520f136e5f4b543b3febee25cf
phoure <phoure.capuchino@msn.com>
2018-05-21 07:41:56 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -49,11 +49,7 @@ if ($err) {
 } else {
     
  echo '<img src="base64,'.base64_encode( $response ).'"/>';
-    // echo '<video width="320" height="240" controls>
-  <source src="movie.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
-Your browser does not support the video tag.
-</video>';
+
   /*
     $tempfile = tmpfile();
     fwrite($tempfile, $response->getRawBody());
