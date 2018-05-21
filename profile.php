a66fd6a278aa977e7fb128cb05d2e1c5b96889d7
phoure <phoure.capuchino@msn.com>
2018-05-21 08:04:24 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -55,13 +55,15 @@ echo '<img src="data:image/jpeg;base64,'.base64_encode( $response ).'"/>';
  </audio>';
     */
  
- $data = 'data:image/jpeg;base64,'.base64_encode( $response );
-
-list($type, $data) = explode(';', $data);
-list(, $data)      = explode(',', $data);
-$data = base64_decode($data);
-
-file_put_contents('aaa.jpg', $data);
+// requires php5
+	define('UPLOAD_DIR', '');
+	$img = $response;
+	$img = str_replace('data:image/jpeg;base64,', '', $img);
+	$img = str_replace(' ', '+', $img);
+	$data = base64_decode($img);
+	$file = UPLOAD_DIR . uniqid() . '.jpeg';
+	$success = file_put_contents($file, $data);
+	print $success ? $file : 'Unable to save the file.';
     
     
 }
